a9c1ab1cc07d82327d03c956bea10319a859a357
yyyuui223 <yyyuui223@example.com>
2026-03-18 01:44:37 +0700

    เปิดพอร์ต 0.0.0.0 ให้ Render เข้าได้สัส

--- app.py
+++ app.py
@@ -307,4 +307,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=1137)
+    app.run(debug=True, host='0.0.0.0', port=10000)
